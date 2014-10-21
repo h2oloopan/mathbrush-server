503f13a0cfeab99b1303be4e2a244cd0283ceaf0
Shengying Pan <i@thepans.me>
2014-10-21 11:50:57 -0400

    this is actually some good progress

--- server/addons/recognizer.cc
+++ server/addons/recognizer.cc
@@ -9,14 +9,24 @@
 
 using namespace v8;
 
-static Local<Function> logger;
+Persistent<Object> logger;
 
 void log(const std::string msg) {
 	const unsigned argc = 1;
 	Local<Value> argv[argc] = {
 		Local<Value>::New(String::New(msg.c_str()))
 	};
-	logger->Call(Context::GetCurrent()->Global(), argc, argv);
+	Local<Function> func = Local<Function>::Cast(logger->Get(String::NewSymbol("log")));
+	func->Call(Context::GetCurrent()->Global(), argc, argv);
+}
+
+void log(const char* msg) {
+	const unsigned argc = 1;
+	Local<Value> argv[argc] = {
+		Local<Value>::New(String::New(msg))
+	};
+	Local<Function> func = Local<Function>::Cast(logger->Get(String::NewSymbol("log")));
+	func->Call(Context::GetCurrent()->Global(), argc, argv);
 }
 
 //input signature: name, strokes, return latex
@@ -25,13 +35,29 @@ Handle<Value> Recognize(const Arguments& args) {
 	Local<String> name = String::New(*String::Utf8Value(args[0]->ToString()));
 	Local<Array> strokes = Local<Array>::Cast(args[1]);
 	String::Utf8Value str(name);
-	std::cout << std::string(*str);
+	log("*********************");
+	log("* Start Recognition");
+	log(std::string("* User: ") + std::string(*str));
+	log("* Input:");
+	for (int i = 0; i < strokes->Length(); i++) {
+		log("* Stroke:");
+		Local<Array> stroke = Local<Array>::Cast(strokes->Get(i));
+		for (int j = 0; j < stroke->Length(); j++) {
+			Local<Array> points = Local<Array>::Cast(stroke->Get(j));
+			Local<Number> x = Local<Number>::Cast(points->Get(0));
+			Local<Number> y = Local<Number>::Cast(points->Get(1));
+			log(std::string("- x: ") + std::string(*(String::Utf8Value(x->ToString()))) + 
+				std::string(" y: ") + std::string(*(String::Utf8Value(y->ToString()))));
+		}
+	}
+
 	return scope.Close(name);
 }
 
 Handle<Value> Initialize(const Arguments& args) {
 	HandleScope scope;
-	logger = Local<Function>::Cast(args[0]);
+	logger = Persistent<Object>::New(args[0]->ToObject());
+	return scope.Close(Null());
 }
 
 void Init(Handle<Object> exports) {
--- server/apis/recognizer.coffee
+++ server/apis/recognizer.coffee
@@ -1,11 +1,29 @@
 recognizer = require '../addons/build/Release/recognizer'
 
-log = (msg) ->
-	console.log msg
 
-recognizer.initialize log
+logger =
+	log: (msg) ->
+		console.log msg
+
+recognizer.initialize logger
 
 exports.bind = (app) ->
 	app.get '/api/recognize/recognize', (req, res) ->
-		result = recognizer.recognize 'pan', [1, 2, 3, 4]
+		input = [
+			[
+				[1, 2]
+				[2, 4]
+				[3, 6]
+				[4, 8]
+				[5, 10]
+			]
+			[
+				[7, 10]
+				[9, 13]
+				[15, 63]
+			]
+		]
+
+
+		result = recognizer.recognize 'pan', input
 		res.send 200, result
